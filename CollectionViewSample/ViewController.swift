a89f993ff14161b48682e217eb4cfd2bd6ab16b7
koyo <kakiage@gmail.com>
2020-01-03 21:39:34 +0900

    Initial Commit

---
+++ CollectionViewSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewSample
+//
+//  Created by koyo on 2020/01/03.
+//  Copyright © 2020 koyo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
